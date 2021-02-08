673e3b59a8e5ecdb778b28469d7c60f85f2eb6d5
RishabhRawat98 <rbrawat98@outlook.com>
2021-02-08 23:20:19 +0000

    added basic templates

--- kirito.py
+++ kirito.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "ZA WAURDO.........TOKI WO TOMARE"
+    return render_template('home.html')
 
 
 @app.route("/discover")
