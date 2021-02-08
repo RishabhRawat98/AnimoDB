737be0ba1122678b2a0b32f221bafa5e95631b87
RishabhRawat98 <rbrawat98@outlook.com>
2021-02-08 23:09:15 +0000

    basic rouute and debug env

--- kirito.py
+++ kirito.py
@@ -1,7 +1,16 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "ZA WAURDO.........TOKI WO TOMARE"
 
+
+@app.route("/discover")
+def discover():
+    return "ELLO ELLO"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
