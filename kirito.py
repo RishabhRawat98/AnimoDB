3d4efa8149d00ed8b124ee1f333173c7efa1f939
Rishabh <rbrawat98@outlook.com>
2021-02-09 12:00:31 +0000

    added some dummy data to test the templates

--- kirito.py
+++ kirito.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+movie_info = [
+    {
+        'id': 1,
+        'title': 'Za Waurdo',
+        'description': 'Dio Brando and his gang try to stop Jotaro Kujo and the boys from smashing thier heads in',
+        'Release_date': '2012'
+    },
+    {
+        'id': 2,
+        'title': 'Killer Queen',
+        'description': 'My name is Yoshikage Kira',
+        'Release_date': '2012'
+    }
+ 
+]
+
+
+
+
+
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', movinfo = movie_info)
 
 
 @app.route("/discover")
